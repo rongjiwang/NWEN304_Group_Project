17e2c54cf93e3a7bfdc20bfd433e4ebe4b73dceb
Kevin <rongji_wang@yahoo.co.nz>
2017-05-29 13:07:18 +1200

    Completed database structure

--- Database/Schema.js
+++ Database/Schema.js
@@ -9,13 +9,56 @@ var pg = require('pg').native
 client = new pg.Client(connectionString);
 client.connect();
 
-query = client.query('CREATE TABLE Account(userId serial primary key, ' +
-    'username varchar(255), password varchar(255), emailaddress varchar(255), admin boolean);');
+query = client.query('' +
+    'DROP TABLE IF EXISTS CART;' +
+    'DROP TABLE IF EXISTS ITEM;' +
+    'DROP TABLE IF EXISTS ACCOUNT;'
+    );
 
-query = client.query('CREATE TABLE ITEM(itemID SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, ' +
-    'description VARCHAR(255), price NUMERIC NOT NULL, quantity INTEGER NOT NULL)');
+query = client.query('' +
+    'CREATE TABLE IF NOT EXISTS Account(' +
+    'userId serial primary key, ' +
+    'username varchar(255) NOT NULL, ' +
+    'password varchar(255) NOT NULL, ' +
+    'emailaddress varchar(255) NOT NULL, ' +
+    'admin boolean NOT NULL);'
+);
 
-query = client.query('CREATE TABLE CART(cartID INTEGER, itemID INTEGER, quantity INTEGER, ' +
-    'PRIMARY KEY (cartID), FOREIGN KEY (cartID) REFERENCES Account (userId))');
+query = client.query('' +
+    'CREATE TABLE IF NOT EXISTS ITEM(' +
+    'itemID SERIAL PRIMARY KEY, ' +
+    'name VARCHAR(255) NOT NULL, ' +
+    'description VARCHAR(255), ' +
+    'price NUMERIC NOT NULL, ' +
+    'quantity INTEGER NOT NULL)');
 
+query = client.query('' +
+    'CREATE TABLE IF NOT EXISTS CART(' +
+    'cartID INTEGER,' +
+    'itemID INTEGER,' +
+    'quantity INTEGER, ' +
+    'PRIMARY KEY (cartID), ' +
+    'FOREIGN KEY (cartID) REFERENCES Account (userId))');
+
+query = client.query('CREATE OR REPLACE FUNCTION trigger_id() ' +
+    'RETURNS TRIGGER AS $$ ' +
+    'BEGIN ' +
+    'INSERT INTO CART(cartid) VALUES(NEW.userid);' +
+    'RETURN NULL;' +
+    'END;' +
+    '$$ LANGUAGE '+'plpgsql'+';');
+
+query = client.query('' +
+    'CREATE TRIGGER trigger_id_action ' +
+    'AFTER INSERT ON ACCOUNT ' +
+    'FOR EACH ROW ' +
+    'EXECUTE PROCEDURE trigger_id();');
+
+//----------Testing Query------------
+query = client.query('insert into account(' +
+    'userid,username,password,emailaddress,admin) ' +
+    'values (default,$1,$2,$3,$4)'
+    ,['Rongji','12345','rong@ttt.com',true]);
+
+//----------Close database connection--------
 query.on('end', () => {client.end(); });
