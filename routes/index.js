b0f0b5ad8b980bc2ab40583b91ff6d8cc3a9fba0
rafa-ela <raftabay@gmail.com>
2017-06-01 14:02:36 +1200

    Separated html files into smaller ones

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NBA Final is coming!!!' });
+  res.render('index', { 
+  	title: 'NBA Final is coming!!!' 
+  });
 });
 
 module.exports = router;
