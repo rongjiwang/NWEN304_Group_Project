2af02980037da066e785a9000c8a9040e804e150
Kevin <rongji_wang@yahoo.co.nz>
2017-06-20 19:23:27 +1200

    complete searching feature

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var db = require('../Database/config');
 router.get('/', function (req, res, next) {
     db.any('select * from item').then(data => {
         //console.log(data);
-        res.render('index', {title: 'Lift-Style', data: data});
+        res.render('index', {title: 'Lift-Style', data: data, message: '', hasResult: true});
 
     }).catch(error => {
         console.log('Error: ' + error);
@@ -15,4 +15,28 @@ router.get('/', function (req, res, next) {
 
 });
 
+router.post('/search', function (req, res, next) {
+    console.log(req.body.search);
+    var searchResult = [];
+    db.any('select * from item')
+        .then(data => {
+            //console.log(data);
+            data.forEach(item => {
+                if (item.name.toLowerCase().includes(req.body.search.toLowerCase()))
+                    searchResult.push(item);
+            });
+            if (searchResult.length == 0)
+                res.render('index', {
+                    title: 'Lift-Style', data: searchResult,
+                    message: 'No relevant search result', hasResult: false
+                });
+            else
+                res.render('index', {title: 'Lift-Style', data: searchResult, message: '', hasResult: true});
+
+        })
+        .catch(error => {
+            console.log('Error: ' + error);
+        });
+
+});
 module.exports = router;
