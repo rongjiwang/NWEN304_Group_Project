8ccffd15fb06091349297ccb73cd99f693f26031
Kevin <rongji_wang@yahoo.co.nz>
2017-06-21 14:19:06 +1200

    fixed facebook callback link from local to heroku url

--- Database/auth.js
+++ Database/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth' :{
         'clientID': '176052999598367',
         'clientSecret':'cd9289f16698560e3f735034c8d35573',
-        'callbackURL':'https://localhost:8000/user/auth/facebook/callback',
+        'callbackURL':'https://desolate-basin-26059.herokuapp.com/user/auth/facebook/callback',
         'profileFields': ['emails']
     }
 }
